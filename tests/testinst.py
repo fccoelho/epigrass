a9c95befdc0e37a5c3d0ff8ad3bbf9619883dbf7
fccoelho <fccoelho@fccoelho-MacBookPro>
2012-11-03 10:04:36 -0200

    added tests for model run

--- tests/testinst.py
+++ tests/testinst.py
@@ -9,6 +9,10 @@ class testObjInstantiation(unittest.TestCase):
         self.sitios = loadData('sitios3.csv',sep=',')
         self.ed=loadData('edgesout.csv',sep=',')
         self.S=simulate('flu.epg')
+    def tearDown(self):
+        if os.path.exists('demos/outdata-flu'):
+            os.rmdir('demos/outdata-flu')
+
     def testSites(self):
         l = self.S.instSites(self.sitios)
         for i in range(len(l)):
@@ -27,5 +31,23 @@ class testObjInstantiation(unittest.TestCase):
         g = self.S.instGraph('grafo',1,l,e)
         self.assertEquals(len(g.site_list), len(self.sitios))
         self.assertEquals(len(g.edge_list),len(self.ed))
+
+class TestSimulationRuns(unittest.TestCase):
+    def setUp(self):
+        os.chdir('demos/')
+#        self.sitios3 = loadData('sitios3.csv',sep=',')
+#        self.nodes = loadData('nodes.csv',sep=',')
+#        self.edgesout = loadData('edgesout.csv',sep=',')
+        pass
+
+    def test_mesh_epg(self):
+        S = simulate('mesh.epg',silent=True)
+        S.start()
+    def tearDown(self):
+        if os.path.exists('outdata-mesh'):
+            os.system('rm -rf outdata-mesh')
+        os.chdir('..')
+
+
 if __name__ == '__main__':
     unittest.main()
