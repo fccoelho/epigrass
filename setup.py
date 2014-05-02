8990b28aa90c439578395ecd63ca85bd9aa01ebe
fccoelho <fccoelho@flavio-ultra>
2014-05-02 12:43:52 -0300

    Added requirements.txt fixing test_models

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding:utf8 -*-
 import ez_setup
+
 ez_setup.use_setuptools()
 from setuptools import setup
 from glob import glob
@@ -10,33 +11,34 @@ from Epigrass.__version__ import version
 demos = glob('demos/*')
 try:
     demos.remove('demos/CVS')
-except: pass
+except:
+    pass
 
 setup(name='epigrass',
-    version=version,
-    author = 'Flavio Codeco Coelho, Claudia Torres Codeco',
-    author_email = 'fccoelho@gmail.com',
-    maintainer = 'Flavio Codeco Coelho',
-    maintainer_email = 'fccoelho@gmail.com',
-    url = 'http://epigrass.sourceforge.net',
-    description = 'Epidemiological Geo-Referenced Analysis and Simulation System',
-    long_description = 'EpiGrass is a simulator of epidemics over networks.  Its is a scientific tool created for simulations and scenario analysis in Network epidemiology.',
-    download_url = 'http://sourceforge.net/project/showfiles.php?group_id=128000',
-    license = 'GPL',
-    packages = ['Epigrass'],
-    install_requires = ["numpy >= 1.2",  "networkx >= 1.1", "SQLAlchemy >= 0.7", "sqlsoup", "redis >= 2.4"],
-    entry_points = {
-        'console_scripts': [
-            'epirunner = Epigrass.manager:main',
-        ],
-        'gui_scripts': [
-            'epigrass = Epigrass.epigrass:main',
-            'epgeditor= Epigrass.epgeditor:main', 
-            'neteditor= Epigrass.neteditor:main'
-        ]
-    }, 
+      version=version,
+      author='Flavio Codeco Coelho, Claudia Torres Codeco',
+      author_email='fccoelho@gmail.com',
+      maintainer='Flavio Codeco Coelho',
+      maintainer_email='fccoelho@gmail.com',
+      url='http://epigrass.sourceforge.net',
+      description='Epidemiological Geo-Referenced Analysis and Simulation System',
+      long_description='EpiGrass is a simulator of epidemics over networks.  Its is a scientific tool created for simulations and scenario analysis in Network epidemiology.',
+      download_url='http://sourceforge.net/project/showfiles.php?group_id=128000',
+      license='GPL',
+      packages=['Epigrass'],
+      install_requires=["numpy >= 1.2", "networkx >= 1.1", "SQLAlchemy >= 0.7", "sqlsoup", "redis >= 2.4"],
+      entry_points={
+          'console_scripts': [
+              'epirunner = Epigrass.manager:main',
+          ],
+          'gui_scripts': [
+              'epigrass = Epigrass.epigrass:main',
+              'epgeditor= Epigrass.epgeditor:main',
+              'neteditor= Epigrass.neteditor:main'
+          ]
+      },
 
-    include_package_data = True,
-    package_data = {'':['INSTALL','README','COPYING','epigrass.desktop','*.rst','*.tex','*.png','*.jpg']},
-    #data_files = [('/usr/share/pixmaps',['egicon.png']),('/usr/share/doc/epigrass/demos',demos),('/usr/share/doc/epigrass/',['docs/build/latex/Epigrass.pdf']),('/usr/share/applications',['epigrass.desktop'])]
-    )
+      include_package_data=True,
+      package_data={'': ['INSTALL', 'README', 'COPYING', 'epigrass.desktop', '*.rst', '*.tex', '*.png', '*.jpg']},
+      #data_files = [('/usr/share/pixmaps',['egicon.png']),('/usr/share/doc/epigrass/demos',demos),('/usr/share/doc/epigrass/',['docs/build/latex/Epigrass.pdf']),('/usr/share/applications',['epigrass.desktop'])]
+)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,73 +1,161 @@
 __author__ = 'fccoelho'
 
-
 import unittest
 from Epigrass.manager import *
-from Epigrass.simobj import siteobj,graph,edge
+from Epigrass.simobj import siteobj, graph, edge
 from Epigrass.epimodels import Epimodel
+
 
 class TestModels(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_run_SIS(self):
-        model = Epimodel("SIS")
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.})
-        self.assertAlmostEqual(res,([0,0.9,999.11],0,0.9))
+        model = Epimodel(1, modtype="SIS")
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1.})
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
         #TOdo: add asserts with the expected results of the models
+
     def test_run_SIS_s(self):
-        model = Epimodel('SIS_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.})
-        self.assertAlmostEqual(res,([0,0.9,999.11],0,0.9))
+        model = Epimodel(1, modtype='SIS_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1.})
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SIR(self):
-        model = Epimodel('SIR')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.})
-        self.assertAlmostEqual(res,([0,0.9,999.01],0,0.9))
+        model = Epimodel(1, modtype='SIR')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1.})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 1)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SIR_s(self):
-        model = Epimodel('SIR_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.})
+        model = Epimodel(1, modtype='SIR_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1.})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 1)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SEIS(self):
-        model = Epimodel('SEIS')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.})
+        model = Epimodel(1, modtype='SEIS')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1.})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SEIS_s(self):
-        model = Epimodel('SEIS_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.})
+        model = Epimodel(1, modtype='SEIS_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1.})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SEIR(self):
-        model = Epimodel('SEIR')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.})
+        model = Epimodel(1, modtype='SEIR')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1.})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SEIR_s(self):
-        model = Epimodel('SEIR_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.})
+        model = Epimodel(1, modtype='SEIR_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1.})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_SIpRpS(self):
-        model = Epimodel('SIpRpS')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.,'delta':0.1})
+        model = Epimodel(1, modtype='SIpRpS')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1., 'delta': 0.1})
+
+        for x, y in zip(res[0], [0, 0.9, 999.01]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0, 1)
+        self.assertAlmostEqual(res[2], 0.9, 1)
+
     def test_run_Influenza(self):
-        model = Epimodel('Influenza')
-        res = model.step((999,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),0,1000,0,0,bi={'e1':0,'is1':1,'s1':999,'e2':0,'is2':1,'s2':0,'e3':0,'is3':1,'s3':0,'e4':0,'is4':1,'s4':0,'ic1':0,'ic2':0,'ic3':0,'ic4':0,'ig1':0,'ig2':0,'ig3':0,'ig4':0},bp={'r':0.1,'b':0.01,'beta':0.01,'e':0.1,'pc1':.1,'c':0.1,'pp1':.1,'g':.1,'d':.1,'pc2':.1,'pp2':.1,'pc3':.1,'pp3':.1,'pc4':.1,'pp4':.1,'alpha':1.,'delta':0.1,'vaccineNow':1,'vaccov':0.3})
+        model = Epimodel(1, modtype='Influenza')
+        res = model.step((999, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 0, 1000, 0, 0,
+                         bi={'e1': 0, 'is1': 1, 's1': 999, 'e2': 0, 'is2': 1, 's2': 0, 'e3': 0, 'is3': 1, 's3': 0,
+                             'e4': 0, 'is4': 1, 's4': 0, 'ic1': 0, 'ic2': 0, 'ic3': 0, 'ic4': 0, 'ig1': 0, 'ig2': 0,
+                             'ig3': 0, 'ig4': 0},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'e': 0.1, 'pc1': .1, 'c': 0.1, 'pp1': .1, 'g': .1,
+                             'd': .1, 'pc2': .1, 'pp2': .1, 'pc3': .1, 'pp3': .1, 'pc4': .1, 'pp4': .1, 'alpha': 1.,
+                             'delta': 0.1, 'vaccineNow': 1, 'vaccov': 0.3})
+        print res
+
+        for x, y in zip(res[0], [699.3065035, 0.0034964999999999996, 0.9, 0.010000000000000002, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0]):
+            self.assertAlmostEqual(x, y, 0)
+        self.assertAlmostEqual(res[1], 0.003496, 3)
+        self.assertAlmostEqual(res[2], 0.46, 2)
+
     def test_run_SIpRpS_s(self):
-        model = Epimodel('SIpRpS_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.,'delta':0.1})
+        model = Epimodel(1, modtype='SIpRpS_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1., 'delta': 0.1})
+
     def test_run_SEIpRpS(self):
-        model = Epimodel('SEIpRpS')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.,'delta':0.1})
+        model = Epimodel(1, modtype='SEIpRpS')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1., 'delta': 0.1})
+
     def test_run_SEIpRpS_s(self):
-        model = Epimodel('SEIpRpS_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.,'delta':0.1})
+        model = Epimodel(1, modtype='SEIpRpS_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1., 'delta': 0.1})
+
     def test_run_SIpR(self):
-        model = Epimodel('SIpR')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.,'delta':0.1,'p':0.1})
+        model = Epimodel(1, modtype='SIpR')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1., 'delta': 0.1, 'p': 0.1})
+
     def test_run_SIpR_s(self):
-        model = Epimodel('SIpR_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.,'delta':0.1,'p':0.1})
+        model = Epimodel(1, modtype='SIpR_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1., 'delta': 0.1, 'p': 0.1})
+
     def test_run_SEIpR(self):
-        model = Epimodel('SEIpR')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.,'delta':0.1,'p':0.1})
+        model = Epimodel(1, modtype='SEIpR')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1., 'delta': 0.1, 'p': 0.1})
+
     def test_run_SEIpR_s(self):
-        model = Epimodel('SEIpR_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'e':0.1,'beta':0.01,'alpha':1.,'delta':0.1,'p':0.1})
+        model = Epimodel(1, modtype='SEIpR_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'e': 0.1, 'beta': 0.01, 'alpha': 1., 'delta': 0.1, 'p': 0.1})
+
     def test_run_SIRS(self):
-        model = Epimodel('SIRS')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.,'delta':0.1,'p':0.1,'w':0.1})
+        model = Epimodel(1, modtype='SIRS')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1., 'delta': 0.1, 'p': 0.1, 'w': 0.1})
+
     def test_run_SIRS_s(self):
-        model = Epimodel('SIRS_s')
-        res = model.step((0,1,999),0,1000,0,0,bi={'e':0,'i':1,'s':999},bp={'r':0.1,'b':0.01,'beta':0.01,'alpha':1.,'delta':0.1,'p':0.1,'w':0.1})
+        model = Epimodel(1, modtype='SIRS_s')
+        res = model.step((0, 1, 999), 0, 1000, 0, 0, bi={'e': 0, 'i': 1, 's': 999},
+                         bp={'r': 0.1, 'b': 0.01, 'beta': 0.01, 'alpha': 1., 'delta': 0.1, 'p': 0.1, 'w': 0.1})
