4ea181c7d2ebacfc85c1e1088c2ca4ce4169e378
Flavio C. Coelho <fccoelho@gmail.com>
2016-10-12 19:08:04 -0300

    Added test to epigdal and made sure shapefiles generated have projection set to EPSG 4326

---
+++ tests/test_epigdal.py
@@ -0,0 +1,39 @@
+import unittest
+from Epigrass.epigdal import World, KmlGenerator
+import os
+
+
+class TestWorld(unittest.TestCase):
+    def test_instantiation(self):
+        w = World('../riozonas_LatLong.shp', 'nome_zonas', 'zona_trafe')
+        layer = w.ds.GetLayerByName(w.layerlist[0])
+        w.get_node_list(layer)
+        w.close_sources()
+        self.assertIsInstance(w, World)
+        self.assertGreater(len(w.nlist), 0)
+
+    def test_create_node_layer(self):
+        w = World('../riozonas_LatLong.shp', 'nome_zonas', 'zona_trafe')
+        layer = w.ds.GetLayerByName(w.layerlist[0])
+        w.get_node_list(layer)
+        w.create_node_layer()
+        w.close_sources()
+        assert os.path.exists('Nodes.shp')
+        assert os.path.exists('Nodes.prj')
+        os.unlink('Nodes.shp')
+        os.unlink('Nodes.shx')
+        os.unlink('Nodes.dbf')
+        os.unlink('Nodes.prj')
+
+    @unittest.skip
+    def test_kml(self):
+        w = World('../riozonas_LatLong.shp', 'nome_zonas', 'zona_trafe')
+        layer = w.ds.GetLayerByName(w.layerlist[0])
+        w.get_node_list(layer)
+        kmlg = KmlGenerator()
+        kmlg.addNodes(layer)
+        kmlg.writeToFile('.')
+
+
+if __name__ == '__main__':
+    unittest.main()
