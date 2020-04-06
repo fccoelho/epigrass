6c39f2fc1ab9bcd5d1da9487e7e8655701f7c0c1
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-04-06 10:46:57 -0300

    Updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ copyright = '2008, Flávio Codeço Coelho'
 # The short X.Y version.
 version = '2.6'
 # The full version, including alpha/beta/rc tags.
-release = '2.6.1'
+release = '2.6.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
