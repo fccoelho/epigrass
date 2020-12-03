c6fa81a1d95058bf8e272248f32dce0f73a1be70
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-12-03 10:56:53 -0300

    fixed more bugs in tests

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,9 +1,12 @@
 import unittest
 from Epigrass.manager import Simulate
 import geopandas as gpd
+import os
+import pathlib
 
 class SimulateTestCase(unittest.TestCase):
     def test_Instantiate(self):
+        os.chdir(pathlib.Path(__file__).parent.absolute())
         S = Simulate(fname='SEIR.epg', backend='sqlite')
         self.assertIsInstance(S, Simulate)
         self.assertIsInstance(S.World.nodes, gpd.GeoDataFrame)
