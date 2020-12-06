eff4c8cdf4dd47351e2c7beee43eb742770b6b13
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-12-06 16:04:09 -0300

    Minor bug-fix minor version bump-up

--- Epigrass/manager.py
+++ Epigrass/manager.py
@@ -926,7 +926,7 @@ def onStraightRun(args):
     """
     from Epigrass import epipanel
     if args.view_only:
-        pth = os.path.join(os.getcwd() + f'outdata-{args.epg[0].split(".")[0]}')
+        pth = os.path.join(os.getcwd() + f'/outdata-{args.epg[0].split(".")[0]}')
         epipanel.show(pth)
     if args.backend == "mysql":
         S = Simulate(fname=args.epg[0], host=args.dbhost, user=args.dbuser, password=args.dbpass, backend=args.backend)
